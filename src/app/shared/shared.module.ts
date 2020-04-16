396c86947619e9ba35b9ba96e9a8dcce2164a743
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-04-16 18:11:07 +0200

    Added new services and modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BannerComponent],
   imports: [
     CommonModule
   ]
