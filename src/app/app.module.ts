07240aa2122526dafb2e7687104d130446a97a51
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-04-16 23:17:00 +0200

    Added Footer and HeadFoot Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
+import { HeadfootModule } from './headfoot/headfoot.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    HeadfootModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
