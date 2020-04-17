af32eb06f95a2a05179c390bce46a375d9465d30
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-04-17 22:18:33 +0200

    Added titles for auth pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SignUpComponent, LogInComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule],
   exports: [SignUpComponent, LogInComponent],
 })
 export class AuthModule {}
