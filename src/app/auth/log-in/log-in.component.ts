5520649ad298b3494e9ae3cf47fb8b60914dbea9
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-04-17 18:04:43 +0200

    Added Both auth forms

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.css'],
 })
 export class LogInComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  logInForm: FormGroup;
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.logInForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  onSubmit() {}
 }
